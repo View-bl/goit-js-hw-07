174fde47b458a280bf4c90ad54177ed7bc95ae52
Roman <romaitgo@gmail.com>
2024-11-04 17:07:16 +0100

    js7

--- js/task-6.js
+++ js/task-6.js
@@ -9,11 +9,10 @@ const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
 
-// Функція для створення квадратів
 function createBoxes(amount) {
   const boxes = [];
   for (let i = 0; i < amount; i++) {
-    const size = 30 + i * 10; // Розмір квадрата (30px + 10px * i)
+    const size = 30 + i * 10;
     const box = document.createElement('div');
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
